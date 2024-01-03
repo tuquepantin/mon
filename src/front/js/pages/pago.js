0a145ad1977d8f7139e7c9b3fc1e6c287cf400d3
Victor Pantin <125493189+tuquepantin@users.noreply.github.com>
2024-01-03 19:10:21 +0000

    casi casi

--- src/front/js/pages/pago.js
+++ src/front/js/pages/pago.js
@@ -1,14 +1,25 @@
 import React, { useState } from "react";
 
 const Pago = () => {
-  const [numero, setNumero] = useState(Math.floor(Math.random() * (3000 - 2500 + 1)) + 2500);
+    const [numero, setNumero] = useState(Math.floor(Math.random() * (3000 - 2500 + 1)) + 2500);
 
-  return (
-    <div>
-      <h1>Generar número aleatorio</h1>
-      <p>Número aleatorio: {numero}</p>
-    </div>
-  );
+    return (
+
+        <div className="container text-center mx-auto">
+            <h4>Datos</h4>
+
+
+            <div className="alert alert-info m-5">
+
+
+                <div>
+                    <h1>Monto a Pagar</h1>
+                    <h2>{numero}Bss</h2>
+                </div>
+            </div>
+
+        </div>
+    );
 };
 
 export default Pago;
